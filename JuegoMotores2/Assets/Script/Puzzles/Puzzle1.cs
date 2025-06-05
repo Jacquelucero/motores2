99f7a854d06466ffdd848f941b850fa22d3e5e9e
Shanluki <155746065+Shanluki@users.noreply.github.com>
2025-06-05 11:54:14 -0300

    Sistema Puzzle 1
    Termine los codigos para el puzzle 1 y añadi una carpeta prefabs para meter el puzzle en funcionamiento

--- JuegoMotores2/Assets/Script/Puzzles/Puzzle1.cs
+++ JuegoMotores2/Assets/Script/Puzzles/Puzzle1.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 
 public class Puzzle1 : MonoBehaviour
 {
-    
+
+
+    public float leverTimeCooldown;
+    [Header("Combinacion correcta")]
+
+    [SerializeField] private bool Lever1;
+    [SerializeField] private bool Lever2;
+    [SerializeField] private bool Lever3;
+    [SerializeField] private bool Lever4;
 
     
+
    
 
-    public bool[] isActive = new bool[4];
+    public bool[] leverState = new bool[4];
 
-    private void Start()
+    private void Update()
     {
-        
-
-       
-            
+        if (leverState[0] == Lever1 && leverState[1] == Lever2 && leverState[2] == Lever3 && leverState[3] == Lever4)
+        {
+            ResolvePuzzle();
+        }
     }
-   
+
+    void ResolvePuzzle()
+    {
+        Debug.Log("Resolviste el puzzle");
+    }
 }
--- JuegoMotores2/Assets/Script/Puzzles/Puzzle1_LeverController.cs
+++ JuegoMotores2/Assets/Script/Puzzles/Puzzle1_LeverController.cs
@@ -7,22 +7,49 @@ public class Puzzle1_LeverController : MonoBehaviour
     [SerializeField] private Puzzle1 Puzzle1;
     [SerializeField] private int thisLeverNumber;
 
-    private bool isActive;
-    private void OnTriggerStay2D(Collider2D collision)
+    
+    private float timeElapsed;
+   
+    private bool canPress = false;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && Input.GetKey(KeyCode.E))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            if (!isActive)
-            {
-                Debug.Log("Activaste la palanca numero: " + thisLeverNumber);
-                Puzzle1.isActive[thisLeverNumber] = true;
-                isActive = true;
-            }
-            else
-            {
-                Debug.Log("Desactivaste la palanca numero: " + thisLeverNumber);
-                Puzzle1.isActive[thisLeverNumber] = false;
-            }
+            canPress = true;
         }
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            canPress = false;
+        }
+    }
+
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (canPress && Input.GetKey(KeyCode.E) && Puzzle1.leverState[thisLeverNumber] == false && timeElapsed >= Puzzle1.leverTimeCooldown)
+        {
+
+            Debug.Log("Activaste la palanca numero: " + thisLeverNumber);
+            Puzzle1.leverState[thisLeverNumber] = true;
+            timeElapsed = 0;
+                             
+        }
+
+       if (canPress && Input.GetKey(KeyCode.E) && Puzzle1.leverState[thisLeverNumber] == true && timeElapsed >= Puzzle1.leverTimeCooldown)
+        {
+
+           Debug.Log("Destivaste la palanca numero: " + thisLeverNumber);
+            Puzzle1.leverState[thisLeverNumber] = false;
+            timeElapsed = 0;
+
+
+        }
+    }
+
+
 }
